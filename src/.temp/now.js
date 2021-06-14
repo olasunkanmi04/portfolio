8972532c946c5d35072bd45f147fac289e2aad43
Olasunkanmi <alabiolasunkanmi@outlook.com>
2021-06-14 19:56:22 +0100

    fixes

--- src/.temp/now.js
+++ src/.temp/now.js
@@ -1,1 +1,1 @@
-export default 1623687352962
+export default 1623696925056
--- src/.temp/plugins-server.js
+++ src/.temp/plugins-server.js
@@ -1,3 +1,8 @@
+import plugin_gridsome_plugin_netlify_cms_6 from "/Users/mac/OneDrive/New Resume/portfolio/node_modules/gridsome-plugin-netlify-cms/gridsome.client.js"
 
 export default [
+  {
+    run: plugin_gridsome_plugin_netlify_cms_6,
+    options: {"publicPath":"/admin","modulePath":"src/admin/index.js","htmlTitle":"Content Editor","configPath":"src/admin/config.yml","htmlPath":"/Users/mac/OneDrive/New Resume/portfolio/node_modules/gridsome-plugin-netlify-cms/templates/index.html","injectScript":true,"enableIdentityWidget":true,"debug":false}
+  }
 ]
